fe83e78d00b9bbab23d4110ee878d6dc6effc02b
cooperbraun13 <cooperjbraun13@gmail.com>
2025-10-04 10:37:15 -0700

    fix later: running into some errors that i will address later

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,31 @@
+// tailwind.config.ts
 import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       fontFamily: {
-        cormorantGaramond: ["var(--font-cormorant-garamond)", "serif"],
-        poppins: ["var(--font-poppins)", "sans-serif"],
+        poppins: [
+          "var(--font-poppins)",
+          "ui-sans-serif",
+          "system-ui",
+          "sans-serif",
+        ],
+        cormorantGaramond: [
+          "var(--font-cormorant-garamond)",
+          "ui-serif",
+          "Georgia",
+          "serif",
+        ],
       },
     },
   },
   plugins: [],
 };
+
 export default config;
