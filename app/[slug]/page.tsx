ff0f708245bd48ea85f9911debf347ee3148ac34
cooperbraun13 <cooperjbraun13@gmail.com>
2025-10-05 01:19:45 -0700

    fix: fixed a lot of bugs due to older tailwind styles (update from v3 to v4) and touched up some of the other code

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -1,10 +1,9 @@
 import Link from "next/link";
-import { ArrowLeftIcon } from "@heroicons/react/16/solid";
+import { ArrowLeftIcon } from "@heroicons/react/24/solid";
 import { getArticleData } from "@/lib/articles";
 
-const Article = async ({ params }: { params: Promise<{ slug: string }> }) => {
-  const { slug } = await params;
-  const articleData = await getArticleData(slug);
+const Article = async ({ params }: { params: { slug: string } }) => {
+  const articleData = await getArticleData(params.slug);
 
   return (
     <section className="mx-auto w-10/12 md:w-1/2 mt-20 flex flex-col gap-5">
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,30 +1,11 @@
-// tailwind.config.ts
 import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {
-    extend: {
-      fontFamily: {
-        poppins: [
-          "var(--font-poppins)",
-          "ui-sans-serif",
-          "system-ui",
-          "sans-serif",
-        ],
-        cormorantGaramond: [
-          "var(--font-cormorant-garamond)",
-          "ui-serif",
-          "Georgia",
-          "serif",
-        ],
-      },
-    },
-  },
   plugins: [],
 };
 
